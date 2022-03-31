90100402cbb00c0aabc510d41dbdebfc959b4516
joon500 <maliar.kursk@gmail.com>
2022-03-31 12:26:54 +0300

    new1

--- src/test/java/TestClassJo.java
+++ src/test/java/TestClassJo.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
@@ -21,10 +22,10 @@ public class TestClassJo {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\jon\\Desktop\\java\\chromedriver2.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS);//все вызовы элементов driver.findElement()
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//все вызовы элементов driver.findElement()
         // будут продолжаться то тех пор,
         // пока элемент не будет найден или будет достигунта граница времени ожидания.
-      //  driver.manage().window().maximize();
+        //  driver.manage().window().maximize();
         System.out.println("Установка законченна");
     }
 
@@ -34,7 +35,7 @@ public class TestClassJo {
         driver.get("https://yandex.ru/");
 
         String locatorForSearchInput = "//input[@class=\"input__control input__input mini-suggest__input\"]";
-        driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS);//все вызовы элементов driver.findElement() до нахождения элемента
+     //   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//все вызовы элементов driver.findElement() до нахождения элемента
         driver.findElement(By.xpath(locatorForSearchInput)).sendKeys("ruby");
     /*    WebElement weit  =    new WebDriverWait(driver, Duration.ofSeconds(5))
          .until(ExpectedConditions.presenceOfElementLocated(By.className("main-locationWrapper-R8itV")));// ожиданеи элемента если нету то пауза*/
@@ -42,38 +43,40 @@ public class TestClassJo {
         //получеие атрибута
         String findAttribute = driver.findElement(By.xpath(locatorForSearchInput)).getAttribute("value");
 
-        Assert.assertEquals(findAttribute,"ruby","занчение не эквевалентно заданому");
-
+        Assert.assertEquals(findAttribute, "ruby", "занчение не эквевалентно заданому");
 
 
     }
+
     @Test
     //сравнение стекста в элементе с заданным
-    public void getButton(){
+    public void getButton() {
         String locatorButton = "//button[@class='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']";
 
-        Assert.assertEquals(driver.findElement(By.xpath(locatorButton)).getText().equals("Найти"),true,"несоответсвует заданному значению");
+        Assert.assertEquals(driver.findElement(By.xpath(locatorButton)).getText().equals("Найти"), true, "несоответсвует заданному значению");
     }
 
     @Test
 //получение текста новости и проверка его
     public void twoTest() throws InterruptedException {
         driver.get("https://kursk.ru/");
-        String  getLocator = "//div[@class=\"news-widget\"]/div[1]/a[1]/span[1]";
+        String getLocator = "//div[@class=\"news-widget\"]/div[1]/a[1]/span[1]";
         String getNews = driver.findElement(By.xpath(getLocator)).getText();
-      //  Assert.assertEquals(getNews,"Поступило несколько сотен заявок от представителей курского бизнеса для участия в программе льготного кредитования");
+         // Assert.assertEquals(getNews,"Поступило несколько сотен заявок от представителей курского бизнеса для участия в программе льготного кредитования");
 
     }
+
     @Test
 //запрос и проверка результата
     public void threeTest() throws InterruptedException {
         driver.get("https://kursk.ru/");
-String getLocator = "//main[@class=\"main\"]//input[@name='q']";
-String getNewsLocator ="//div[@class=\"app\"]/main/div/div/div/div[2]/a[1]/span";
- driver.findElement(By.xpath(getLocator)).sendKeys("проект", Keys.ENTER);
- String  getNews = driver.findElement(By.xpath(getNewsLocator)).getText();
 
-        Assert.assertEquals(getNews.contains("Об утверждении"),true);
+        String getLocator = "//main[@class=\"main\"]//input[@name='q']";
+        String getNewsLocator = "//div[@class=\"app\"]/main/div/div/div/div[2]/a[1]/span";
+        driver.findElement(By.xpath(getLocator)).sendKeys("проект", Keys.ENTER);
+        String getNews = driver.findElement(By.xpath(getNewsLocator)).getText();
+
+        Assert.assertEquals(getNews.contains("Об утверждении"), true);
 
 
     }
@@ -82,7 +85,7 @@ String getNewsLocator ="//div[@class=\"app\"]/main/div/div/div/div[2]/a[1]/span"
     @AfterSuite
     public void cleanUp() {
         //  driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));//поосто ожидание
-       driver.quit();
+        driver.quit();
         System.out.println("Все активноcти закрыты");
     }
 }
